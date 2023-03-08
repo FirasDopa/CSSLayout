3b64e6925c86581a6e15538b8b74b8e36a3df188
FirasDopa <mohamedfiras.silini@esprit.tn>
2023-03-08 23:32:26 +0100

    challenge nael

--- main.js
+++ main.js
@@ -1,29 +1,43 @@
-let mybutton = document.getElementById("btn-back-to-top");
+function ToTop(){
+    window.scrollTo({top : 0, behavior : "smooth"})
+   }
+     let slideIndex = 1;
+     showSlides(slideIndex);
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function scrollToTop () {
-    if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
-        window.scrollTo({
-            top : 5 ,
-            behavior : "smooth"
-        })}
-scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+     // Next/previous controls
+     function plusSlides(n) {
+     showSlides(slideIndex += n);
 }
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", function() {
-    scrollToTop();
-});
-window.addEventListener("click",function(){
-    scrollFunction();
-})
+
+// Thumbnail image controls
+function currentSlide(n) {
+ showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+ let i;
+ let slides = document.getElementsByClassName("mySlides");
+ let dots = document.getElementsByClassName("dot");
+ if (n > slides.length) {slideIndex = 1}
+ if (n < 1) {slideIndex = slides.length}
+ for (i = 0; i < slides.length; i++) {
+   slides[i].style.display = "none";
+ }
+ for (i = 0; i < dots.length; i++) {
+   dots[i].className = dots[i].className.replace(" active", "");
+ }
+ slides[slideIndex-1].style.display = "block";
+ dots[slideIndex-1].className += " active";
+}
+const Form = document.getElementById("myform")
+const Btn = document.getElementById("btnform")
+Form.addEventListener("input", Validation)
+Form.addEventListener("change", Validation)
+function Validation(){
+ if (Form.checkValidity()){
+   Btn.disabled = false;
+ }
+ else {
+   Btn.disabled = true;
+ }
+}
